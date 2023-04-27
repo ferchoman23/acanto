d11732fb3bbf6f9894e4a65ecfbf9a215e31b6b2
Angel Méndez <79428530+angelmendez43@users.noreply.github.com>
2023-04-27 12:06:14 -0600

    Add files via upload
    Cambiando de crm_extended a Acanto

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Acanto",
+    'summary': """
+        Desarrollo para acanto""",
+    'description': """
+       Desarrollo para acanto
+    """,
+    'author': "ST",
+    'website': "",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['crm','base','account'],
+    'data': [
+        'views/crm_lead_views.xml',
+        'security/security.xml',  
+    ],
+    'demo': [    
+    ],
+    'installable': True,
+    'aplication' : False,
+}
